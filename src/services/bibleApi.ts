5b2030cbab95c61e3a04d36781d75e15ae91ca91
Likhith32 <likhithmanakala@gmail.com>
2026-02-22 09:18:46 +0530

    Update bibleApi.ts

--- src/services/bibleApi.ts
+++ src/services/bibleApi.ts
@@ -1,7 +1,6 @@
 import { BiblePassage } from "@/data/bibleData";
 
-const API_URL = import.meta.env.VITE_API_URL || "http://127.0.0.1:8000";
-
+const API_URL = import.meta.env.VITE_API_URL || "https://bible-backend-nvcb.onrender.com";
 interface ApiVerseResponse {
   verse: number;
   textEnglish: string | null;
